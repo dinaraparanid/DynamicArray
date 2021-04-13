f451d05ddc88ddb2473b91302726831ee6c1340f
Paranid5 <dinaraparanid@gmail.com>
2021-04-14 00:56:45 +0500

    fp

--- DynamicArray/dynamic_array.c
+++ DynamicArray/dynamic_array.c
@@ -1,9 +1,9 @@
-﻿#include "dynamic_array.h"
-
-#include <stdint.h>
+﻿#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+#include "dynamic_array.h"
 
 typedef struct array_data
 {
@@ -12,24 +12,6 @@ typedef struct array_data
 	void* __data__;
 	size_t __size_of_elem__;
 } array_data;
-
-static size_t __array_data_size__ = sizeof(array_data);
-
-void* __get_from_dyn_array__(const dynamic_array arr, const size_t ind)
-{
-	if (ind >= arr.arr_data->__size__)
-	{
-		fprintf(stderr, "Index out of range: %zu >= %zu", ind, arr.arr_data->__size__);
-		exit(1);
-	}
-
-	int8_t* getter = arr.arr_data->__data__;
-	return getter + ind * arr.arr_data->__size_of_elem__;
-}
-
-#define get_casted(ARR, IND, TYPE)	*(TYPE*)(ARR).get(ARR, IND)				
-
-size_t __size_of_dyn_array__(const dynamic_array arr) { return arr.arr_data->__size__; }
 
 void __swap__(void* p1, void* p2, const size_t size_of_elem)
 {
@@ -42,7 +24,7 @@ void __swap__(void* p1, void* p2, const size_t size_of_elem)
 	free(buffer);
 }
 
-inline void* __ptr_check__(void* p)
+void* __ptr_check__(void* p)
 {
 	if (p == NULL)
 	{
@@ -53,6 +35,30 @@ inline void* __ptr_check__(void* p)
 	return p;
 }
 
+size_t __calc_capacity__(const size_t size)
+{
+	size_t capacity = 1;
+
+	while (1 << capacity < size)
+		capacity++;
+
+	return capacity;
+}
+
+void* __get_from_dyn_array__(const dynamic_array arr, const size_t ind)
+{
+	if (ind >= arr.arr_data->__size__)
+	{
+		fprintf(stderr, "Index out of range: %zu >= %zu", ind, arr.arr_data->__size__);
+		exit(1);
+	}
+
+	int8_t* getter = arr.arr_data->__data__;
+	return getter + ind * arr.arr_data->__size_of_elem__;
+}			
+
+size_t __size_of_dyn_array__(const dynamic_array arr) { return arr.arr_data->__size__; }
+
 void __push_to_dyn_array__(dynamic_array* arr, void* elem)
 {
 	if (arr->arr_data->__size__ + 1 > 1 << arr->arr_data->__capacity__)
@@ -62,14 +68,24 @@ void __push_to_dyn_array__(dynamic_array* arr, void* elem)
 	__swap__(arr_elem, elem, arr->arr_data->__size_of_elem__);
 }
 
+void __push_cloned_to_dyn_array__(dynamic_array* arr, const void* elem)
+{
+	
+	const size_t size_of_elem = arr->arr_data->__size_of_elem__;
+
+	void* clone = malloc(size_of_elem);
+	memmove(clone, elem, size_of_elem);
+
+	__push_to_dyn_array__(arr, clone);
+}
+
 void __pop_from_dyn_array__(dynamic_array* arr) {
 	arr->arr_data->__size__--;
 }
 
 void __shrink_to_fit_dyn_array__(dynamic_array* arr)
 {
-	while (1 << arr->arr_data->__capacity__ >= arr->arr_data->__size__)
-		arr->arr_data->__capacity__--;
+	arr->arr_data->__capacity__ = __calc_capacity__(arr->arr_data->__size__);
 
 	arr->arr_data->__data__ = __ptr_check__(realloc(arr->arr_data->__data__, ++arr->arr_data->__capacity__ * arr->arr_data->__size_of_elem__));
 }
@@ -126,13 +142,24 @@ void* __bin_search_rule_dyn_array__(const dynamic_array arr, const void* key, in
 	return bsearch(key, arr.arr_data->__data__, arr.arr_data->__size__, arr.arr_data->__size_of_elem__, cmp);
 }
 
-size_t __find_dyn_array__(const dynamic_array arr, const void* key)
+size_t __find_first_dyn_array__(const dynamic_array arr, const void* key)
 {
 	int8_t* data = arr.arr_data->__data__;
 
 	for (size_t i = 0; i < arr.arr_data->__size__; i++)
 		if (memcmp(data + i * arr.arr_data->__size_of_elem__, key, arr.arr_data->__size_of_elem__) == 0)
 			return i;
+
+	return arr.arr_data->__size__;
+}
+
+size_t __find_last_dyn_array__(const dynamic_array arr, const void* key)
+{
+	int8_t* data = arr.arr_data->__data__;
+
+	for (size_t i = arr.arr_data->__size__; i > 0; i--)
+		if (memcmp(data + (i - 1) * arr.arr_data->__size_of_elem__, key, arr.arr_data->__size_of_elem__) == 0)
+			return i - 1;
 
 	return arr.arr_data->__size__;
 }
@@ -147,29 +174,174 @@ array_data* __data_with_elems__(const size_t capacity, const size_t amount, cons
 	return d;
 }
 
+dynamic_array* __filter_dyn_array__(const dynamic_array arr, int (*f)(const void*))
+{
+	const size_t size_of_elem = arr.arr_data->__size_of_elem__;
+	dynamic_array* filtered = new_dyn_array(size_of_elem);
+	int8_t* data = arr.arr_data->__data__;
+
+	for (size_t i = 0; i < arr.arr_data->__size__; i++)
+	{
+		void* elem = data + i * size_of_elem;
+		if (f(elem)) filtered->push(filtered, elem);
+	}
+
+	return filtered;
+}
+
+void* __fold_dyn_array__(const dynamic_array arr, const void* start, const size_t size_of_start, void*(*act)(const void*, const void*))
+{
+	void* cpy = malloc(size_of_start);
+	memmove(cpy, start, size_of_start);
+
+	int8_t* data = arr.arr_data->__data__;
+	
+	for (size_t i = 0; i < arr.arr_data->__size__; i++)
+	{
+		void* res = act(cpy, data + i * arr.arr_data->__size_of_elem__);
+		memmove(cpy, res, size_of_start);
+		free(res);
+	}
+
+	return cpy;
+}
+
+dynamic_array* __scan_dyn_array__(const dynamic_array arr, const void* start, const size_t size_of_start, void* (*act)(const void*, const void*))
+{
+	dynamic_array* ans = new_dyn_array_with_capacity(size_of_start, arr.arr_data->__capacity__);
+	void* cpy = malloc(size_of_start);
+	memmove(cpy, start, size_of_start);
+
+	int8_t* data = arr.arr_data->__data__;
+
+	for (size_t i = 0; i < arr.arr_data->__size__; i++)
+	{
+		void* res = act(cpy, data + i * arr.arr_data->__size_of_elem__);
+		memmove(cpy, res, size_of_start);
+		free(res);
+
+		ans->push_cloned(ans, cpy);
+	}
+
+	return ans;
+}
+
+dynamic_array* __sub_dyn_array__(const dynamic_array arr, const size_t from, const size_t to)
+{
+	if (to >= arr.arr_data->__size__)
+	{
+		fprintf(stderr, "Sub array construction: 'to' index out of range %zu >= %zu", to, arr.arr_data->__size__);
+		exit(1);
+	}
+
+	if (from > to)
+	{
+		fprintf(stderr, "Sub array construction: 'from' %zu > 'to' %zu", from, to);
+		exit(1);
+	}
+
+	const size_t size_of_elem = arr.arr_data->__size_of_elem__;
+	const size_t total_size = to - from;
+	size_t total_capacity = 1;
+
+	while (1 << total_capacity < total_size)
+		total_capacity++;
+
+	array_data* data = __data_with_elems__(
+		total_capacity,
+		total_size,
+		size_of_elem
+	);
+
+	int8_t* arr_data = arr.arr_data->__data__;
+	memmove(data->__data__, arr_data + to * size_of_elem, total_size * size_of_elem);
+
+	dynamic_array* sub = malloc(__dynamic_array_size__);
+	sub->arr_data->__data__ = arr_data;
+	__set_methods_dyn_array__(sub);
+	
+	return sub;
+}
+
+dynamic_array* __clone_dyn_array__(const dynamic_array arr)
+{
+	array_data* data = __data_with_elems__(
+		arr.arr_data->__capacity__,
+		arr.arr_data->__size__,
+		arr.arr_data->__size_of_elem__
+	);
+
+	memmove(data->__data__, arr.arr_data->__data__, data->__size_of_elem__ * arr.arr_data->__size__);
+
+	dynamic_array* clone = malloc(__dynamic_array_size__);
+	clone->arr_data = data;
+	__set_methods_dyn_array__(clone);
+
+	return clone;
+}
+
+void __reverse_dyn_array__(dynamic_array* arr)
+{
+	const size_t size = arr->arr_data->__size__;
+	const size_t size_of_elem = arr->arr_data->__size_of_elem__;
+	int8_t* data = arr->arr_data->__data__;
+	
+	for (size_t i = 0; i < size / 2; i++)
+		__swap__(data + i * size_of_elem, data + (size - i - 1) * size_of_elem, size_of_elem);
+}
+
+dynamic_array* __reversed_dyn_array__(const dynamic_array arr)
+{
+	const size_t size = arr.arr_data->__size__;
+	const size_t size_of_elem = arr.arr_data->__size_of_elem__;
+	int8_t* data = arr.arr_data->__data__;
+
+	array_data* arr_data = __data_with_elems__(__calc_capacity__(size), size, size_of_elem);
+	
+	int8_t* arr_d = arr.arr_data->__data__;
+	int8_t* data_d = arr_data->__data__;
+	
+	for (size_t i = 0; i < size; i++)
+		memmove(data_d + i * size_of_elem, arr_d + (size - 1 - i) * size_of_elem, size_of_elem);
+
+	dynamic_array* reversed = malloc(__dynamic_array_size__);
+	reversed->arr_data = arr_data;
+	__set_methods_dyn_array__(reversed);
+
+	return reversed;
+}
+
 void* __get_data_dyn_array__(dynamic_array* arr) { return arr->arr_data->__data__; }
 size_t __get_size_of_elem__(dynamic_array* arr) { return arr->arr_data->__size_of_elem__; }
 
-dynamic_array* new_dyn_array(const size_t size_of_elem)
+void __set_methods_dyn_array__(dynamic_array* arr)
 {
-	array_data* data = malloc(sizeof(array_data));
-	data->__capacity__ = 1;
-	data->__size__ = 0;
-	data->__data__ = malloc(size_of_elem);
-	data->__size_of_elem__ = size_of_elem;
-
-	dynamic_array* arr = malloc(sizeof(dynamic_array));
-	arr->arr_data = data;
 	arr->size = __size_of_dyn_array__;
 	arr->get = __get_from_dyn_array__;
 	arr->push = __push_to_dyn_array__;
+	arr->push_cloned = __push_cloned_to_dyn_array__;
 	arr->pop = __pop_from_dyn_array__;
 	arr->shrink_to_fit = __shrink_to_fit_dyn_array__;
 	arr->insert = __insert_to_dyn_array__;
 	arr->remove = __remove_by_in_from_dyn_array__;
 	arr->sort_by = __sort_rule_dyn_array__;
 	arr->bin_search_by = __bin_search_rule_dyn_array__;
-	arr->find = __find_dyn_array__;
+	arr->find_first = __find_first_dyn_array__;
+	arr->find_last = __find_last_dyn_array__;
+	arr->filter = __filter_dyn_array__;
+	arr->fold = __fold_dyn_array__;
+	arr->scan = __scan_dyn_array__;
+	arr->clone = __clone_dyn_array__;
+	arr->sub_array = __sub_dyn_array__;
+	arr->reverse = __reverse_dyn_array__;
+	arr->reversed = __reversed_dyn_array__;
+}
+
+dynamic_array* new_dyn_array(const size_t size_of_elem)
+{
+	dynamic_array* arr = malloc(__dynamic_array_size__);
+	arr->arr_data = __data_with_elems__(1, 0, size_of_elem);
+	__set_methods_dyn_array__(arr);
 
 	return arr;
 }
@@ -182,24 +354,23 @@ dynamic_array* new_dyn_array_with_capacity(const size_t size_of_elem, const uint
 		exit(1);
 	}
 
-	array_data* data = malloc(sizeof(array_data));
-	data->__capacity__ = capacity;
-	data->__size__ = 0;
-	data->__data__ = malloc((1 << capacity) * size_of_elem);
-	data->__size_of_elem__ = size_of_elem;
+	dynamic_array* arr = malloc(__dynamic_array_size__);
+	arr->arr_data = __data_with_elems__(capacity, 0, size_of_elem);
+	__set_methods_dyn_array__(arr);
 
-	dynamic_array* arr = malloc(sizeof(dynamic_array));
-	arr->arr_data = data;
-	arr->size = __size_of_dyn_array__;
-	arr->get = __get_from_dyn_array__;
-	arr->push = __push_to_dyn_array__;
-	arr->pop = __pop_from_dyn_array__;
-	arr->shrink_to_fit = __shrink_to_fit_dyn_array__;
-	arr->insert = __insert_to_dyn_array__;
-	arr->remove = __remove_by_in_from_dyn_array__;
-	arr->sort_by = __sort_rule_dyn_array__;
-	arr->bin_search_by = __bin_search_rule_dyn_array__;
-	arr->find = __find_dyn_array__;
+	return arr;
+}
+
+dynamic_array* new_dyn_array_with_elem(const size_t size_of_elem, const void* elem, const size_t amount)
+{
+	dynamic_array* arr = malloc(__dynamic_array_size__);
+	arr->arr_data = __data_with_elems__(__calc_capacity__(amount), amount, size_of_elem);
+	__set_methods_dyn_array__(arr);
+
+	int8_t* data = arr->arr_data->__data__;
+
+	for (size_t i = 0; i < arr->arr_data->__size__; i++)
+		memmove(data + i * size_of_elem, elem, size_of_elem);
 
 	return arr;
 }
--- DynamicArray/dynamic_array.h
+++ DynamicArray/dynamic_array.h
@@ -2,7 +2,6 @@
 #define DYNAMICARRAY_H
 
 #include <stdint.h>
-#include <stdio.h>
 
 typedef struct array_data array_data;
 
@@ -10,20 +9,31 @@ typedef struct dynamic_array
 {
 	array_data* arr_data;
 
-	size_t(*size)(struct dynamic_array self);
 	void* (*get)(struct dynamic_array self, size_t ind);
+	void* (*bin_search_by)(struct dynamic_array self, const void* key_ptr, int (*cmp)(const void* arg1, const void* arg2));
+	void* (*fold)(struct dynamic_array self, const void* start, size_t size_of_start, void* (*act)(const void* accumulator, const void* elem));
+	
 	void (*push)(struct dynamic_array* self, void* elem);
+	void (*push_cloned)(struct dynamic_array* self, const void* elem);
 	void (*pop)(struct dynamic_array* self);
 	void (*shrink_to_fit)(struct dynamic_array* self);
 	void (*insert)(struct dynamic_array* self, size_t, void* elem);
 	void (*remove)(struct dynamic_array* self, size_t ind);
-	void (*sort_by)(struct dynamic_array* self, int (*cmp)(const void*, const void*));
-	void* (*bin_search_by)(struct dynamic_array self, const void* key_ptr, int (*cmp)(const void*, const void*));
-	size_t(*find)(struct dynamic_array self, const void* key_ptr);
+	void (*sort_by)(struct dynamic_array* self, int (*cmp)(const void* arg1, const void* arg2));
+	void (*reverse) (struct dynamic_array* self);
+	
+	size_t(*size)(struct dynamic_array self);
+	size_t(*find_first)(struct dynamic_array self, const void* key_ptr);
+	size_t(*find_last)(struct dynamic_array self, const void* key_ptr);
+
+	struct dynamic_array* (*filter) (struct dynamic_array self, int (*f) (const void* elem));
+	struct dynamic_array* (*scan) (struct dynamic_array self, const void* start, size_t size_of_start, void* (*act)(const void* accumulator, const void* elem));
+	struct dynamic_array* (*clone) (struct dynamic_array self);
+	struct dynamic_array* (*sub_array) (struct dynamic_array self, size_t from, size_t to);
+	struct dynamic_array* (*reversed) (struct dynamic_array self);
 
 } dynamic_array;
 
-extern size_t __array_data_size__;
 static size_t __dynamic_array_size__ = sizeof(dynamic_array);
 
 int __cmp_int8_t__(const void* arg1, const void* arg2);
@@ -36,23 +46,15 @@ int __cmp_int64_t__(const void* arg1, const void* arg2);
 int __cmp_uint64_t__(const void* arg1, const void* arg2);
 int __cmp_str__(const void* arg1, const void* arg2);
 
-void __push_to_dyn_array__(dynamic_array* arr, void* elem);
-void __pop_from_dyn_array__(dynamic_array* arr);
-void __shrink_to_fit_dyn_array__(dynamic_array* arr);
-void __insert_to_dyn_array__(dynamic_array* arr, size_t ind, void* elem);
-void __remove_by_in_from_dyn_array__(dynamic_array* arr, size_t ind);
-void __sort_rule_dyn_array__(dynamic_array* arr, int (*cmp)(const void*, const void*));
-void* __get_from_dyn_array__(dynamic_array arr, size_t ind);
+void __set_methods_dyn_array__(dynamic_array* arr);
 void* __get_data_dyn_array__(dynamic_array* arr);
-void* __bin_search_rule_dyn_array__(dynamic_array arr, const void* key, int (*cmp)(const void*, const void*));
-size_t __size_of_dyn_array__(dynamic_array arr);
-size_t __find_dyn_array__(dynamic_array arr, const void* key);
 size_t __get_size_of_elem__(dynamic_array* arr);
+size_t __calc_capacity__(size_t size);
 
 inline dynamic_array* new_dyn_array(size_t size_of_elem);
 inline dynamic_array* new_dyn_array_with_capacity(size_t size_of_elem, uint8_t capacity);
-
-array_data* __data_with_elems__(size_t capacity, size_t amount, size_t size_of_elem);
+inline dynamic_array* new_dyn_array_with_elem(size_t size_of_elem, const void* elem, size_t amount);
+inline array_data* __data_with_elems__(size_t capacity, size_t amount, size_t size_of_elem);
 
 #define get_casted(ARR_PTR, IND, TYPE)	*(TYPE*)(ARR_PTR)->get(*(ARR_PTR), IND)
 #define sort_dyn_array(ARR_PTR, TYPE) qsort(__get_data_dyn_array__(ARR_PTR), (ARR_PTR)->size(*(ARR_PTR)), __get_size_of_elem__(ARR_PTR), __cmp_##TYPE##__)
@@ -63,25 +65,14 @@ array_data* __data_with_elems__(size_t capacity, size_t amount, size_t size_of_e
 	const size_t __AMOUNT_##ARRAY_NAME##__															\
 		= sizeof(__ARGS##ARRAY_NAME##__) / sizeof(*__ARGS##ARRAY_NAME##__);							\
 	size_t __CAPACITY##ARRAY_NAME##__ = 1;															\
-	while (1 << __CAPACITY##ARRAY_NAME##__ < __AMOUNT_##ARRAY_NAME##__)								\
-		__CAPACITY##ARRAY_NAME##__++;																\
-	array_data* __DATA_##ARRAY_NAME##__  = __data_with_elems__(										\
+	__CAPACITY##ARRAY_NAME##__ = __calc_capacity__(__AMOUNT_##ARRAY_NAME##__);						\
+	dynamic_array* ARRAY_NAME = malloc(__dynamic_array_size__);										\
+	ARRAY_NAME->arr_data = __data_with_elems__(														\
 		__CAPACITY##ARRAY_NAME##__,																	\
 		__AMOUNT_##ARRAY_NAME##__,																	\
 		sizeof(TYPE)																				\
 	);																								\
-	dynamic_array* ARRAY_NAME = malloc(__dynamic_array_size__);										\
-	ARRAY_NAME->arr_data = __DATA_##ARRAY_NAME##__;													\
-	ARRAY_NAME->size = __size_of_dyn_array__;														\
-	ARRAY_NAME->get = __get_from_dyn_array__;														\
-	ARRAY_NAME->push = __push_to_dyn_array__;														\
-	ARRAY_NAME->pop = __pop_from_dyn_array__;														\
-	ARRAY_NAME->shrink_to_fit = __shrink_to_fit_dyn_array__;										\
-	ARRAY_NAME->insert = __insert_to_dyn_array__;													\
-	ARRAY_NAME->remove = __remove_by_in_from_dyn_array__;											\
-	ARRAY_NAME->sort_by = __sort_rule_dyn_array__;													\
-	ARRAY_NAME->bin_search_by = __bin_search_rule_dyn_array__;										\
-	ARRAY_NAME->find = __find_dyn_array__;															\
+	__set_methods_dyn_array__(ARRAY_NAME);															\
 	TYPE* __DATA_OF_ARRAY##ARRAY_NAME##__ = __get_data_dyn_array__(ARRAY_NAME);						\
 	for (size_t i = 0; i < __AMOUNT_##ARRAY_NAME##__; i++)											\
 		__DATA_OF_ARRAY##ARRAY_NAME##__[i] = __ARGS##ARRAY_NAME##__[i]
